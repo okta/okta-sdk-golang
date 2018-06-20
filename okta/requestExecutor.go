6982e3ccb39129b795d3ce4f75150bf09f9a7524
Brian Retterer <bretterer@gmail.com>
2018-06-20 15:08:28 -0400

    Adds content type to header when body is being posted

--- okta/requestExecutor.go
+++ okta/requestExecutor.go
@@ -63,6 +63,9 @@ func (re *RequestExecutor) doRequest(method string, url string, body io.Reader) 
 		return nil, err
 	}
 
+	if (method == "POST" && body != nil) {
+		req.Header.Add("Content-Type", "application/json")
+	}
 	req.Header.Add("Authorization", "SSWS " + re.config.Okta.Client.Token)
 	req.Header.Add("User-Agent", "okta-sdk-golang/0.0.0-development") //TODO: create a User-Agent object to fill and create this on build.
 
