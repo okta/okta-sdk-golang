72411c3e3eba591c454ca0dd9f446cb548b719ab
Brian Retterer <bretterer@gmail.com>
2018-08-22 11:33:44 -0700

    Makes a copy of request when pulling from cache

--- okta/cache/cache.go
+++ okta/cache/cache.go
@@ -17,6 +17,8 @@
 package cache
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -31,4 +33,16 @@ type Cache interface {
 func CreateCacheKey(req *http.Request) string {
 	s := req.URL.Scheme + "://" + req.URL.Host + req.URL.RequestURI()
 	return s
+}
+
+func CopyResponse(resp *http.Response) *http.Response {
+	c := *resp
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return resp
+	}
+
+	c.Body = ioutil.NopCloser(bytes.NewBuffer(respBody))
+
+	return &c
 }
