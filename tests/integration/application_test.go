3a6572d2ead7458503e1e65f3626ccfaadb7b67f
Brian Retterer <bretterer@gmail.com>
2018-09-04 15:05:45 -0400

    WIP

--- tests/integration/application_test.go
+++ tests/integration/application_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/okta/okta-sdk-golang/tests"
 )
 
-func Test_can_create_a_bookmark_applicaiton(t *testing.T) {
+func Test_can_create_a_bookmark_application(t *testing.T) {
 	client := tests.NewClient()
 
 	bookmarkApplicationSettingsApplication := okta.BookmarkApplicationSettingsApplication{
@@ -41,14 +41,42 @@ func Test_can_create_a_bookmark_applicaiton(t *testing.T) {
 
 	bookmarkApplication := okta.NewBookmarkApplication()
 	bookmarkApplication.Settings = &bookmarkApplicationSettings
-
+	assert.Empty(t, bookmarkApplication.Id)
 	application, _, err := client.Application.CreateApplication(bookmarkApplication, nil)
 	require.NoError(t, err, "Creating an application should not error")
 
 	assert.IsType(t, &okta.BookmarkApplication{}, application, "Application type returned was incorrect")
+	assert.NotEmpty(t, application.(*okta.BookmarkApplication).Id)
 
 	client.Application.DeactivateApplication(application.(*okta.BookmarkApplication).Id)
 	_, err = client.Application.DeleteApplication(application.(*okta.BookmarkApplication).Id)
+
+	require.NoError(t, err, "Deleting an application should not error")
+}
+
+func Test_can_create_a_basic_authentication_application(t *testing.T) {
+	client := tests.NewClient()
+
+	basicApplicationSettingsApplication := okta.NewBasicApplicationSettingsApplication()
+	basicApplicationSettingsApplication.AuthURL = "https://example.com/auth.html"
+	basicApplicationSettingsApplication.Url = "https://example.com/auth.html"
+
+	basicApplicationSettings := okta.NewBasicApplicationSettings()
+	basicApplicationSettings.App = basicApplicationSettingsApplication
+
+	basicApplication := okta.NewBasicAuthApplication()
+	basicApplication.Settings = basicApplicationSettings
+
+	assert.Empty(t, basicApplication.Id)
+	application, _, err := client.Application.CreateApplication(basicApplication, nil)
+	require.NoError(t, err, "Creating an application should not error")
+
+	assert.IsType(t, &okta.BasicAuthApplication{}, application, "Application type returned was incorrect")
+	assert.NotEmpty(t, application.(*okta.BasicAuthApplication).Id)
+	assert.NotEmpty(t, basicApplication.Id)
+
+	client.Application.DeactivateApplication(application.(*okta.BasicAuthApplication).Id)
+	_, err = client.Application.DeleteApplication(application.(*okta.BasicAuthApplication).Id)
 
 	require.NoError(t, err, "Deleting an application should not error")
 }
