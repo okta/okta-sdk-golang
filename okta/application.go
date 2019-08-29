a21a4871e22712d7b266da721495cd5bd9c2dfbf
Manjunath Patil <manju754@gmail.com>
2019-08-29 12:52:11 +0530

    Blocker: List Application fails
    List Application fails to unmarshal
    
    "Error: json: cannot unmarshal object into Go value of type okta.App"

--- okta/application.go
+++ okta/application.go
@@ -101,7 +101,7 @@ func (m *ApplicationResource) DeleteApplication(appId string) (*Response, error)
 	}
 	return resp, nil
 }
-func (m *ApplicationResource) ListApplications(qp *query.Params) ([]App, *Response, error) {
+func (m *ApplicationResource) ListApplications(qp *query.Params) ([]interface{}, *Response, error) {
 	url := fmt.Sprintf("/api/v1/apps")
 	if qp != nil {
 		url = url + qp.String()
@@ -111,7 +111,7 @@ func (m *ApplicationResource) ListApplications(qp *query.Params) ([]App, *Respon
 		return nil, nil, err
 	}
 
-	var application []App
+	var application []interface{}
 	resp, err := m.client.requestExecutor.Do(req, &application)
 	if err != nil {
 		return nil, resp, err
