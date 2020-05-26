7bdddd82f50c90a0d86da9502f7cfd32e4982b48
Brian Retterer <bretterer@gmail.com>
2020-05-26 13:37:05 -0400

    WIP

--- okta/requestExecutor.go
+++ okta/requestExecutor.go
@@ -229,18 +229,12 @@ func (re *RequestExecutor) Do(ctx context.Context, req *http.Request, v interfac
 			return nil, err
 		}
 		if resp.StatusCode >= 200 && resp.StatusCode <= 299 && req.Method == http.MethodGet && v != nil && reflect.TypeOf(v).Kind() != reflect.Slice {
-			resp, err := dumpResponse(resp, false)
-			if err != nil {
-				return nil, err
-			}
-
 			re.cache.Set(cacheKey, resp)
 		}
 		return buildResponse(resp, &v)
 	}
 
 	resp := re.cache.Get(cacheKey)
-
 	return buildResponse(resp, &v)
 
 }
@@ -304,63 +298,6 @@ func tryDrainBody(body io.ReadCloser) error {
 		return err
 	}
 	return nil
-}
-
-func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
-	if b == http.NoBody {
-		// No copying needed. Preserve the magic sentinel meaning of NoBody.
-		return http.NoBody, http.NoBody, nil
-	}
-	var buf bytes.Buffer
-	if _, err = buf.ReadFrom(b); err != nil {
-		return nil, b, err
-	}
-	if err = b.Close(); err != nil {
-		return nil, b, err
-	}
-	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
-}
-
-var emptyBody = ioutil.NopCloser(strings.NewReader(""))
-var errNoBody = errors.New("sentinel error value")
-
-type failureToReadBody struct{}
-
-func (failureToReadBody) Read([]byte) (int, error) { return 0, errNoBody }
-func (failureToReadBody) Close() error             { return nil }
-
-func dumpResponse(resp *http.Response, body bool) (*http.Response, error) {
-	var b bytes.Buffer
-	var err error
-	save := resp.Body
-	savecl := resp.ContentLength
-
-	if !body {
-		// For content length of zero. Make sure the body is an empty
-		// reader, instead of returning error through failureToReadBody{}.
-		if resp.ContentLength == 0 {
-			resp.Body = emptyBody
-		} else {
-			resp.Body = failureToReadBody{}
-		}
-	} else if resp.Body == nil {
-		resp.Body = emptyBody
-	} else {
-		save, resp.Body, err = drainBody(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-	}
-	err = resp.Write(&b)
-	if err == errNoBody {
-		err = nil
-	}
-	resp.Body = save
-	resp.ContentLength = savecl
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
 }
 
 func backoffPause(ctx context.Context, retryCount int32, response *http.Response) error {
