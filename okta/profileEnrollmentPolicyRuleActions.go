5e2cc2dc5ccc595ceb6494186275a0bdb83de44f
Mike Mondragon <mikemondragon@gmail.com>
2022-02-16 17:10:49 -0800

    New model IdpPolicyRuleAction

--- okta/profileEnrollmentPolicyRuleActions.go
+++ okta/profileEnrollmentPolicyRuleActions.go
@@ -20,6 +20,7 @@ package okta
 
 type ProfileEnrollmentPolicyRuleActions struct {
 	Enroll                   *PolicyRuleActionsEnroll           `json:"enroll,omitempty"`
+	Idp                      *IdpPolicyRuleAction               `json:"idp,omitempty"`
 	PasswordChange           *PasswordPolicyRuleAction          `json:"passwordChange,omitempty"`
 	SelfServicePasswordReset *PasswordPolicyRuleAction          `json:"selfServicePasswordReset,omitempty"`
 	SelfServiceUnlock        *PasswordPolicyRuleAction          `json:"selfServiceUnlock,omitempty"`
