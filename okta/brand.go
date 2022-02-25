54b7acb8c2a52789400946e881279352e074af7c
Mike Mondragon <mikemondragon@gmail.com>
2022-02-25 12:08:57 -0800

    Email Templates

--- okta/brand.go
+++ okta/brand.go
@@ -25,6 +25,8 @@ import (
 	"io"
 	"mime/multipart"
 	"os"
+
+	"github.com/okta/okta-sdk-golang/v2/okta/query"
 )
 
 type BrandResource resource
@@ -98,6 +100,257 @@ func (m *BrandResource) ListBrands(ctx context.Context) ([]*Brand, *Response, er
 	}
 
 	return brand, resp, nil
+}
+
+// List email templates in your organization with pagination.
+func (m *BrandResource) ListEmailTemplates(ctx context.Context, brandId string, qp *query.Params) ([]*EmailTemplate, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email", brandId)
+	if qp != nil {
+		url = url + qp.String()
+	}
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplate []*EmailTemplate
+
+	resp, err := rq.Do(ctx, req, &emailTemplate)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplate, resp, nil
+}
+
+// Fetch an email template by templateName
+func (m *BrandResource) GetEmailTemplate(ctx context.Context, brandId string, templateName string) (*EmailTemplate, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplate *EmailTemplate
+
+	resp, err := rq.Do(ctx, req, &emailTemplate)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplate, resp, nil
+}
+
+// Delete all customizations for an email template. Also known as “Reset to Default”.
+func (m *BrandResource) DeleteEmailTemplateCustomizations(ctx context.Context, brandId string, templateName string) (*Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("DELETE", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := m.client.requestExecutor.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// List all email customizations for an email template
+func (m *BrandResource) ListEmailTemplateCustomizations(ctx context.Context, brandId string, templateName string) ([]*EmailTemplateCustomization, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateCustomization []*EmailTemplateCustomization
+
+	resp, err := rq.Do(ctx, req, &emailTemplateCustomization)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateCustomization, resp, nil
+}
+
+// Create an email customization
+func (m *BrandResource) CreateEmailTemplateCustomization(ctx context.Context, brandId string, templateName string, body EmailTemplateCustomizationRequest) (*EmailTemplateCustomization, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("POST", url, body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateCustomization *EmailTemplateCustomization
+
+	resp, err := rq.Do(ctx, req, &emailTemplateCustomization)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateCustomization, resp, nil
+}
+
+// Delete an email customization
+func (m *BrandResource) DeleteEmailTemplateCustomization(ctx context.Context, brandId string, templateName string, customizationId string) (*Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations/%v", brandId, templateName, customizationId)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("DELETE", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := m.client.requestExecutor.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// Fetch an email customization by id.
+func (m *BrandResource) GetEmailTemplateCustomization(ctx context.Context, brandId string, templateName string, customizationId string) (*EmailTemplateCustomization, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations/%v", brandId, templateName, customizationId)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateCustomization *EmailTemplateCustomization
+
+	resp, err := rq.Do(ctx, req, &emailTemplateCustomization)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateCustomization, resp, nil
+}
+
+// Update an email customization
+func (m *BrandResource) UpdateEmailTemplateCustomization(ctx context.Context, brandId string, templateName string, customizationId string, body EmailTemplateCustomizationRequest) (*EmailTemplateCustomization, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations/%v", brandId, templateName, customizationId)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("PUT", url, body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateCustomization *EmailTemplateCustomization
+
+	resp, err := rq.Do(ctx, req, &emailTemplateCustomization)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateCustomization, resp, nil
+}
+
+// Get a preview of an email template customization.
+func (m *BrandResource) GetEmailTemplateCustomizationPreview(ctx context.Context, brandId string, templateName string, customizationId string) (*EmailTemplateContent, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/customizations/%v/preview", brandId, templateName, customizationId)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateContent *EmailTemplateContent
+
+	resp, err := rq.Do(ctx, req, &emailTemplateContent)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateContent, resp, nil
+}
+
+// Fetch the default content for an email template.
+func (m *BrandResource) GetEmailTemplateDefaultContent(ctx context.Context, brandId string, templateName string) (*EmailTemplateContent, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/default-content", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateContent *EmailTemplateContent
+
+	resp, err := rq.Do(ctx, req, &emailTemplateContent)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateContent, resp, nil
+}
+
+// Fetch a preview of an email template&#x27;s default content by populating velocity references with the current user&#x27;s environment.
+func (m *BrandResource) GetEmailTemplateDefaultContentPreview(ctx context.Context, brandId string, templateName string) (*EmailTemplateContent, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/default-content/preview", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateContent *EmailTemplateContent
+
+	resp, err := rq.Do(ctx, req, &emailTemplateContent)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateContent, resp, nil
+}
+
+// Send a test email to the current users primary and secondary email addresses. The email content is selected based on the following priority: An email customization specifically for the users locale. The default language of email customizations. The email templates default content.
+func (m *BrandResource) SendTestEmail(ctx context.Context, brandId string, templateName string, body EmailTemplateTestRequest) (*EmailTemplateContent, *Response, error) {
+	url := fmt.Sprintf("/api/v1/brands/%v/templates/email/%v/test", brandId, templateName)
+
+	rq := m.client.CloneRequestExecutor()
+
+	req, err := rq.WithAccept("application/json").WithContentType("application/json").NewRequest("POST", url, body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emailTemplateContent *EmailTemplateContent
+
+	resp, err := rq.Do(ctx, req, &emailTemplateContent)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emailTemplateContent, resp, nil
 }
 
 // List all the themes in your brand
