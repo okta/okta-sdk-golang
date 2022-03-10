6813f24a49263ac45045b60a87ddb642fb1c9ad2
Mike Mondragon <mikemondragon@gmail.com>
2022-03-10 12:13:05 -0800

    Code gen off openapi spec branch redo_apple_idp_values `OPENAPI_SPEC_BRANCH=redo_apple_idp_values make pull-spec` `make generate-files`

--- okta/identityProviderCredentialsSigning.go
+++ okta/identityProviderCredentialsSigning.go
@@ -19,5 +19,7 @@
 package okta
 
 type IdentityProviderCredentialsSigning struct {
-	Kid string `json:"kid,omitempty"`
+	Kid        string `json:"kid,omitempty"`
+	PrivateKey string `json:"privateKey,omitempty"`
+	TeamId     string `json:"teamId,omitempty"`
 }
