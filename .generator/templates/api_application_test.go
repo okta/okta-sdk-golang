4625841716a4d3df7af79edf05ffc973cba92520
Brian Retterer <bretterer@gmail.com>
2022-06-30 15:12:21 -0400

    WIP

---
+++ .generator/templates/api_application_test.go
@@ -0,0 +1,54 @@
+package okta
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var ctx context.Context
+var apiClient *APIClient
+
+func init() {
+	ctx = context.WithValue(
+		context.Background(),
+		ContextAPIKeys,
+		map[string]APIKey{
+			"API_Token": {
+				Key:    "***",
+				Prefix: "SSWS",
+			},
+		},
+	)
+
+	configuration := NewConfiguration()
+	configuration.Debug = false
+
+	apiClient = NewAPIClient(configuration)
+}
+
+func Test_Get_Applications(t *testing.T) {
+
+	t.Run("can get all applicaitons", func(t *testing.T) {
+		req := apiClient.ApplicationApi.ListApplications(ctx).Limit(2)
+		applications, _, _ := req.Execute()
+		assert.Equal(t, 2, len(applications))
+	})
+}
+
+func Test_OIDC_Application(t *testing.T) {
+	t.Run("create an oidc application", func(t *testing.T) {
+		// ctx := context.WithValue(
+		// 	context.Background(),
+		// 	ContextAPIKeys,
+		// 	map[string]APIKey{
+		// 		"api_token": {
+		// 			Key:    "***",
+		// 			Prefix: "SSWS",
+		// 		},
+		// 	},
+		// )
+
+	})
+}
