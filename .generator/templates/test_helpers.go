fa7e4e94ffcd10e0ff771e345d07f65cd91821a6
Tien Nguyen <duytien.nguyen@okta.com>
2022-09-09 14:39:03 -0400

    add it for idp

--- .generator/templates/test_helpers.go
+++ .generator/templates/test_helpers.go
@@ -1,6 +1,7 @@
 package okta
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"time"
@@ -51,6 +52,16 @@ func testName(name string) string {
 	return s
 }
 
+func contain(searchPhrase string, searchList []string) (res bool) {
+	for _, searchResult := range searchList {
+		if searchPhrase == searchResult {
+			res = true
+			break
+		}
+	}
+	return
+}
+
 type TestFactory struct{}
 
 var testFactory TestFactory
@@ -81,4 +92,152 @@ func (t *TestFactory) NewValidTestRecoveryQuestionCredential() *RecoveryQuestion
 	question := "How many roads must a man walk down?"
 	answer := "forty two"
 	return &RecoveryQuestionCredential{Question: &question, Answer: &answer}
+}
+
+func (t *TestFactory) NewValidTestIdentityProvider() *IdentityProvider {
+	res := IdentityProvider{}
+	res.SetType("OIDC")
+	res.SetName(randomTestString())
+	res.SetProtocol(*t.NewValidTestProtocol())
+	res.SetPolicy(*t.NewValidTestIdentityProviderPolicy())
+	return &res
+}
+
+func (t *TestFactory) NewValidTestProtocol() *Protocol {
+	payload := `{
+		"algorithms": {
+			"request": {
+				"signature": {
+					"algorithm": "SHA-256",
+					"scope": "REQUEST"
+				}
+			},
+			"response": {
+				"signature": {
+					"algorithm": "SHA-256",
+					"scope": "ANY"
+				}
+			}
+		},
+		"endpoints": {
+			"acs": {
+				"binding": "HTTP-POST",
+				"type": "INSTANCE"
+			},
+			"authorization": {
+				"binding": "HTTP-REDIRECT",
+				"url": "https://idp.example.com/authorize"
+			},
+			"token": {
+				"binding": "HTTP-POST",
+				"url": "https://idp.example.com/token"
+			},
+			"userInfo": {
+				"binding": "HTTP-REDIRECT",
+				"url": "https://idp.example.com/userinfo"
+			},
+			"jwks": {
+				"binding": "HTTP-REDIRECT",
+				"url": "https://idp.example.com/keys"
+			}
+		},
+		"scopes": [
+			"openid",
+			"profile",
+			"email"
+		],
+		"type": "OIDC",
+		"credentials": {
+			"client": {
+				"client_id": "your-client-id",
+				"client_secret": "your-client-secret"
+			}
+		},
+		"issuer": {
+			"url": "https://idp.example.com"
+		}
+	}`
+	var ptc Protocol
+	err := json.Unmarshal([]byte(payload), &ptc)
+	if err != nil {
+		return nil
+	}
+	return &ptc
+}
+
+func (t *TestFactory) NewValidTestIdentityProviderPolicy() *IdentityProviderPolicy {
+	payload := `{
+		"accountLink": {
+			"action": "AUTO",
+			"filter": null
+		},
+		"provisioning": {
+			"action": "AUTO",
+			"conditions": {
+				"deprovisioned": {
+					"action": "NONE"
+				},
+				"suspended": {
+					"action": "NONE"
+				}
+			},
+			"groups": {
+				"action": "NONE"
+			}
+		},
+		"maxClockSkew": 120000,
+		"subject": {
+			"userNameTemplate": {
+				"template": "idpuser.email"
+			},
+			"matchType": "USERNAME"
+		}
+	
+	}`
+	var policy IdentityProviderPolicy
+	err := json.Unmarshal([]byte(payload), &policy)
+	if err != nil {
+		return nil
+	}
+	return &policy
+}
+
+func (t *TestFactory) NewValidTestJsonWebKey() *JsonWebKey {
+	keys := []string{`@"MIIDnjCCAoagAwIBAgIGAVG3MN+PMA0GCSqGSIb3DQEBBQUAMIGPMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5p
+	YTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEUMBIGA1UECwwLU1NPUHJvdmlkZXIxEDAOBgNVBAMM
+	B2V4YW1wbGUxHDAaBgkqhkiG9w0BCQEWDWluZm9Ab2t0YS5jb20wHhcNMTUxMjE4MjIyMjMyWhcNMjUxMjE4MjIyMzMyWjCB
+	jzELMAkGA1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xDTALBgNVBAoMBE9r
+	dGExFDASBgNVBAsMC1NTT1Byb3ZpZGVyMRAwDgYDVQQDDAdleGFtcGxlMRwwGgYJKoZIhvcNAQkBFg1pbmZvQG9rdGEuY29t
+	MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtcnyvuVCrsFEKCwHDenS3Ocjed8eWDv3zLtD2K/iZfE8BMj2wpTf
+	n6Ry8zCYey3mWlKdxIybnV9amrujGRnE0ab6Q16v9D6RlFQLOG6dwqoRKuZy33Uyg8PGdEudZjGbWuKCqqXEp+UKALJHV+k4
+	wWeVH8g5d1n3KyR2TVajVJpCrPhLFmq1Il4G/IUnPe4MvjXqB6CpKkog1+ThWsItPRJPAM+RweFHXq7KfChXsYE7Mmfuly8s
+	DQlvBmQyxZnFHVuiPfCvGHJjpvHy11YlHdOjfgqHRvZbmo30+y0X/oY/yV4YEJ00LL6eJWU4wi7ViY3HP6/VCdRjHoRdr5L/
+	DwIDAQABMA0GCSqGSIb3DQEBBQUAA4IBAQCzzhOFkvyYLNFj2WDcq1YqD4sBy1iCia9QpRH3rjQvMKDwQDYWbi6EdOX0TQ/I
+	YR7UWGj+2pXd6v0t33lYtoKocp/4lUvT3tfBnWZ5KnObi+J2uY2teUqoYkASN7F+GRPVOuMVoVgm05ss8tuMb2dLc9vsx93s
+	Dt+XlMTv/2qi5VPwaDtqduKkzwW9lUfn4xIMkTiVvCpe0X2HneD2Bpuao3/U8Rk0uiPfq6TooWaoW3kjsmErhEAs9bA7xuqo
+	1KKY9CdHcFhkSsMhoeaZylZHtzbnoipUlQKSLMdJQiiYZQ0bYL83/Ta9fulr1EERICMFt3GUmtYaZZKHpWSfdJp9"`}
+	return &JsonWebKey{X5c: keys}
+}
+
+func (t *TestFactory) NewValidTestCSRMetadata() *CsrMetadata {
+	payload := `{
+		"subject": {
+		  "countryName"            : "US",
+		  "stateOrProvinceName"    : "California",
+		  "localityName"           : "San Francisco",
+		  "organizationName"       : "Okta, Inc.",
+		  "organizationalUnitName" : "Dev",
+		  "commonName"             : "SP Issuer"
+		},
+		"subjectAltNames": 
+		  {
+			"dnsNames": [ "dev.example.com" ] 
+		  }
+	  }`
+	var csrm CsrMetadata
+	err := json.Unmarshal([]byte(payload), &csrm)
+	if err != nil {
+		return nil
+	}
+	return &csrm
 }
