d343b5bf4b099cae5ba637fec607ce340e4e8c5b
Evan do Carmo <carmo.evan@gmail.com>
2022-09-19 16:25:55 -0300

    Update requestExecutor.go

--- okta/requestExecutor.go
+++ okta/requestExecutor.go
@@ -139,9 +139,9 @@ func (re *RequestExecutor) NewRequest(method string, url string, body interface{
 			if re.config.PrivateKeySigner == nil {
 				priv := []byte(strings.ReplaceAll(re.config.Okta.Client.PrivateKey, `\n`, "\n"))
 
-				privPem, err := pem.Decode(priv)
-				if err != nil {
-					return nil, fmt.Errorf("invalid private key: %w", err)
+				privPem, _ := pem.Decode(priv)
+				if privPem == nil {
+					return nil, errors.New("invalid private key")
 				}
 				if privPem.Type != "RSA PRIVATE KEY" {
 					return nil, fmt.Errorf("RSA private key is of the wrong type")
