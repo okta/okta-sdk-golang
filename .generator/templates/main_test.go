3e00e6b70e946f37f82edb63d19aae43bdcb5fc2
Tien Nguyen <duytien.nguyen@okta.com>
2022-09-27 15:48:14 -0400

    add sweep

---
+++ .generator/templates/main_test.go
@@ -0,0 +1,126 @@
+package okta
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+var (
+	apiClient *APIClient
+)
+
+func init() {
+	configuration := NewConfiguration()
+	configuration.Debug = false
+
+	apiClient = NewAPIClient(configuration)
+}
+
+func TestMain(m *testing.M) {
+	err := sweep()
+	if err != nil {
+		fmt.Printf("failed to clean up organization before integration tests: %v", err)
+	}
+	exitVal := m.Run()
+	err = sweep()
+	if err != nil {
+		fmt.Printf("failed to clean up organization after integration tests: %v", err)
+	}
+	os.Exit(exitVal)
+}
+
+func sweep() (err error) {
+	log.Println("[INFO] sweeping test users, groups, idps")
+	err = sweepUsers()
+	if err != nil {
+		return
+	}
+	err = sweepGroups()
+	if err != nil {
+		return
+	}
+	return sweepIdps()
+}
+
+func sweepUsers() error {
+	req := apiClient.UserApi.ListUsers(apiClient.cfg.Context).Limit(200)
+	req = req.Q("SDK_TEST_")
+	users, resp, err := req.Execute()
+	if err != nil {
+		return err
+	}
+	for _, u := range users {
+		if err := cleanUpUser(u.GetId()); err != nil {
+			return err
+		}
+	}
+	for resp.HasNextPage() {
+		var users []*User
+		resp, err = resp.Next(&users)
+		if err != nil {
+			return err
+		}
+		for _, u := range users {
+			if err := cleanUpUser(u.GetId()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func sweepGroups() error {
+	req := apiClient.GroupApi.ListGroups(apiClient.cfg.Context).Limit(200)
+	req = req.Q("SDK_TEST")
+	groups, resp, err := req.Execute()
+	if err != nil {
+		return err
+	}
+	for _, g := range groups {
+		if err := cleanUpGroup(g.GetId()); err != nil {
+			return err
+		}
+	}
+	for resp.HasNextPage() {
+		var groups []*Group
+		resp, err = resp.Next(&groups)
+		if err != nil {
+			return err
+		}
+		for _, g := range groups {
+			if err := cleanUpGroup(g.GetId()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func sweepIdps() error {
+	req := apiClient.IdentityProviderApi.ListIdentityProviders(apiClient.cfg.Context).Limit(200)
+	req = req.Q("SDK_TEST")
+	idps, resp, err := req.Execute()
+	if err != nil {
+		return err
+	}
+	for _, idp := range idps {
+		if err := cleanUpIdp(idp.GetId()); err != nil {
+			return err
+		}
+	}
+	for resp.HasNextPage() {
+		var idps []*IdentityProvider
+		resp, err = resp.Next(&idps)
+		if err != nil {
+			return err
+		}
+		for _, idp := range idps {
+			if err := cleanUpIdp(idp.GetId()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
